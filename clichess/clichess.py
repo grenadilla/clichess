8ca8b691468e5a50c5d326b470efc9270ca413aa
Jeffrey Zhang <jeffreyzhang01@gmail.com>
2020-05-15 13:54:46 -0500

    Increase spacing and fix color inversion

--- clichess/clichess.py
+++ clichess/clichess.py
@@ -24,13 +24,13 @@ def stream():
 
 # Given square position and color perspective, return curses color code
 def print_square(x, y, piece, is_white):
-    square_white = (x + y) % 2 == 1
+    square_white = (x + y) % 2 == 0
     if square_white:
         # White on light
-        print(Fore.BLACK + Back.WHITE + piece, end="")
+        print(Fore.BLACK + Back.WHITE + '\u2007' + piece + '\u2007', end="")
     else:
         # White on dark
-        print(Fore.BLACK + Back.MAGENTA + piece, end="")
+        print(Fore.BLACK + Back.MAGENTA + '\u2007' + piece + '\u2007', end="")
     if x == 7:
         print(Style.RESET_ALL)
 
@@ -39,7 +39,8 @@ def print_square(x, y, piece, is_white):
 # Flips the board if the player !is_white
 def print_board(board, is_white):
     empty = 'O'
-    board_string = board.unicode(invert_color=is_white, empty_square=empty)
+    invert = not is_white
+    board_string = board.unicode(invert_color=invert, empty_square=empty)
     board_string = board_string.replace(' ', '')
     board_string = board_string.replace('\n', '')
     for y in range(0, BOARD_SIZE):
@@ -47,7 +48,8 @@ def print_board(board, is_white):
             # times two since returns string with space between pieces
             # plus 1 from newline character
             if board_string[y * BOARD_SIZE + x] == empty:
-                print_square(x, y, '\u2007', is_white)
+                # \u2007 is a space character
+                print_square(x, y, "\u2007", is_white)
             else:
                 print_square(x, y, board_string[y * BOARD_SIZE + x], is_white)
 
@@ -55,5 +57,6 @@ def print_board(board, is_white):
 if __name__ == '__main__':
     colorama.init()
     board = chess.Board()
+    board.push_san("e4")
     print_board(board, True)
     print(Style.RESET_ALL)
