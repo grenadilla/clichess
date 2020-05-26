3c51811229d2d3924705c30f7e35be1a1e7892fb
Jeffrey Zhang <jeffreyzhang01@gmail.com>
2020-05-26 18:50:08 -0500

    Add check for success in challenges and moves

--- clichess/chesscmd.py
+++ clichess/chesscmd.py
@@ -1,7 +1,7 @@
 import cmd
 import berserk
 import colorama
-from prettyprint import print_board, print_challenges, print_games
+import prettyprint
 from config import Config
 from data_streamer import DataStreamer
 from chess_game import ChessGame
@@ -35,23 +35,23 @@ class ChessCmd(cmd.Cmd):
         self.game_data = None
         self.board = None
 
-    def do_account(self, inp):
+    def do_account(self, args):
         # TODO add pretty printing
         print(self.account)
 
-    def do_board(self, inp):
+    def do_board(self, args):
         '''Updates the game and prints the board'''
         if self.game is None:
             print("Select a game")
             return
         self.game.update_game(self.data_streamer.get_game(self.game.game_id))
-        print_board(self.game.board, self.game.is_white)
+        prettyprint.print_board(self.game.board, self.game.is_white)
         if self.game.is_player_move():
             print("It is your turn")
         else:
             print("Waiting for opponent to move")
 
-    def do_move(self, inp):
+    def do_move(self, args):
         '''Make a move in a game using SAN or UCI format, case sensitive'''
         if self.game is None:
             print("Select a game")
@@ -61,33 +61,34 @@ class ChessCmd(cmd.Cmd):
         else:
             move = self.game.move_player(inp)
             if move is not None:
-                self.client.board.make_move(self.game.game_id, move)
+                if not self.client.board.make_move(self.game.game_id, move):
+                    print("There was an error making your move")
 
-    def do_draw(self, inp):
+    def do_draw(self, args):
         '''Offer a draw in a game'''
         if self.game is None:
             print("Select a game")
             return
-        self.client.board.offer_draw(self.game.game_id)
+        if not self.client.board.offer_draw(self.game.game_id):
+            print("There was an error while offering a draw")
 
-    def do_challenge(self, inp):
-        challenge = self.client.challenges.create(username=inp, rated=False)
-        print(challenge)
+    def do_challenge(self, args):
+        self.client.challenges.create(username=inp, rated=False):
 
-    def do_game(self, inp):
+    def do_game(self, args):
         '''Choose a game to play'''
-        if inp == "":
+        if args == "":
             print("Choose a game")
         else:
             game_id = None
-            if inp.isnumeric():
+            if args.isnumeric():
                 index = int(inp)
                 if index >= len(self.games):
                     print("Index out of bounds")
                 else:
                     game_id = self.games[index]["id"]
-            elif any(d["id"] == inp for d in self.games):
-                game_id = inp
+            elif any(d["id"] == args for d in self.games):
+                game_id = args
             else:
                 print("Invalid game ID")
             if game_id is not None:
@@ -95,28 +96,28 @@ class ChessCmd(cmd.Cmd):
                                       self.data_streamer.get_game(game_id))
                 self.prompt = f"(clichess/{self.game.game_id}) "
 
-    def do_challenges(self, inp):
+    def do_challenges(self, args):
         '''Refresh the list of challenges, then list all challenges'''
         # Get all challenges from queue and convert to local copy
         # to deal with thread issues
         self.challenges = list(self.data_streamer.challenges.queue)
-        print_challenges(self.challenges)
+        prettyprint.print_challenges(self.challenges)
 
-    def do_accept(self, inp):
+    def do_accept(self, args):
         '''Accept a challenge, either by index or challenge ID'''
-        if inp == "":
+        if args == "":
             print("Choose a challenge to accept")
         else:
             challenge_id = None
-            if inp.isnumeric():
+            if args.isnumeric():
                 index = int(inp)
                 if index >= len(self.games):
                     print("Index out of bounds")
                 else:
                     challenge_id = self.challenges[index]["id"]
             # Maybe rewrite this; might be inneficient to keep generating
-            elif any(d["id"] == inp for d in self.challenges):
-                challenge_id = inp
+            elif any(d["id"] == args for d in self.challenges):
+                challenge_id = args
             else:
                 print("Invalid challenge ID")
             if challenge_id is not None:
@@ -125,12 +126,35 @@ class ChessCmd(cmd.Cmd):
                 else:
                     print("There was an error accepting this challenge")
 
-    def do_games(self, inp):
+    def do_decline(self, args):
+        '''Decline a challenge, either by index or challenge ID'''
+        if args == "":
+            print("Choose a challenge to decline")
+        else:
+            challenge_id = None
+            if args.isnumeric():
+                index = int(inp)
+                if index >= len(self.games):
+                    print("Index out of bounds")
+                else:
+                    challenge_id = self.challenges[index]["id"]
+            # Maybe rewrite this; might be inneficient to keep generating
+            elif any(d["id"] == args for d in self.challenges):
+                challenge_id = args
+            else:
+                print("Invalid challenge ID")
+            if challenge_id is not None:
+                if self.client.challenges.decline(challenge_id):
+                    self.data_streamer.delete_challenge(challenge_id)
+                else:
+                    print("There was an error declining this challenge")
+
+    def do_games(self, args):
         '''Refresh the list of games, then list all games'''
         self.games = list(self.data_streamer.games.queue)
-        print_games(self.games)
+        pretty_print.print_games(self.games)
 
-    def do_exit(self, inp):
+    def do_exit(self, args):
         '''Exit the application'''
         return True
 
